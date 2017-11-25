a36194d1fc24f4a386f7a07dea82fe7d35e3a6fe
Juan <juanpessanha@gmail.com>
2017-11-25 12:41:45 -0200

    alterando nome do desafio 25 e removendo arquivo desafio 22

---
+++ Desafio 25.py
@@ -0,0 +1,6 @@
+nome = input('Digite seu nome completo: ')
+normal = nome.lower()
+if 'silva' in normal:
+    print('Você tem um nome muito comum!')
+else:
+    print('Seu nome nem é tão normal assim!')
