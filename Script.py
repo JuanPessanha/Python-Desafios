13fdd7fdde1e704add2913b0e9bf2e991304d9ac
Juan <juanpessanha@gmail.com>
2017-11-25 15:33:29 -0200

    instalado pypiwin32 para que o python possa se conectar com o docker

--- Script.py
+++ Script.py
@@ -1,20 +1,4 @@
-loop = 1
+import docker
+client = docker.from_env()
+print(client.containers.list(all))
 
-print('-=-' * 20)
-print('Bem Vindo ao script python do Juan')
-print('-=-' * 20, '\n')
-
-while loop != 0:
-    comando = input('>>> ')
-
-    if comando == 'ok':
-        print('Rammus\n')
-
-    elif comando == 'amoeba':
-        print('Olha no que deu\n')
-
-    elif comando == 'exit':
-        exit()
-
-    else:
-        print('favor digite um comando válido\n')
